71512aa061789c206731ed2375f32067562efd7b
JAYBEkster <jaybekster@gmail.com>
2014-01-11 01:33:45 +0400

    COMPLETELY CHANGED!!!

--- proga.cpp
+++ proga.cpp
@@ -1,32 +1,104 @@
 #include <stdio.h>
 #include <iostream>
 #include <cstdlib>
+#include <assert.h>
 
 using namespace std;
 
-struct Node
-{
+class Node {
 	int data;
-	Node* next;
+	Node *next;
+public:
+	Node() {}
+	Node(int arg1) {
+		data = arg1;
+	}
+	int getData() {
+		return data;
+	}
+	void setNext(Node* node) {
+		next = node;
+		return;
+	}
+	void setData(int new_data) {
+		data = new_data;
+		return;
+	}
+	Node* getNext() {
+		return next;
+	}
+	~Node() {}
 };
-struct Queue {
-	int size;
+
+class Queque {
 	Node* head; // голова односвязного списка
 	Node* tail; // хвост односвязного списка
 public: 
-	int cnt; // длина очереди (кол-во элементов в ней)
-	Queue() {
-		cnt = 0;
+	Queque() {
 		head = tail = NULL;
 	};
-	bool is_empty() {
+	bool is_empty() const {
 		return head == NULL;
 	}; 
-	Queue& del() { // удалить элемент из начала очереди
-		if ( is_empty() ) {
-			cout << "Stack is empty";
-		};
-		Node* tmp = head->next; // запоминаем указатель на следующий элемент очереди
+	Queque& push(const int data) { // добавить элемент в конец очереди
+		Node* node = new Node(data);
+		if (!head) {
+			head = tail = node;
+		} else {
+			tail->setNext(node);
+			tail = node;
+		}
+		return *this;
+	};
+	Queque& pop(const int data) { // добавить элемент в неачало очереди
+		Node* node = new Node(data);
+		if (!head) {
+			head = tail = node;
+		} else {
+			node->setNext(head);
+			head = node;
+		}
+		return *this;
+	};
+	Node* getFirst() {
+		return head;
+	}
+	Node* getLast() {
+		return tail;
+	}
+	int getFirstData() {
+		return head->getData();
+	}
+	int getLastData() {
+		return tail->getData();
+	}
+	Queque& setFirstData(const int data) {
+		head->setData(data);
+		return *this;
+	}
+	Queque& setLastData(const int data) {
+		tail->setData(data);
+		return *this;
+	}
+	Node* getn(int ind) {
+		Node* node = head;
+		int i = 0;
+		if (ind==0) return head;
+		while (node->getNext()) {
+			node = node->getNext();
+			i+=1;
+			if (i==ind) return node;
+		}
+		return NULL;
+	}
+	Queque& clear() {
+		// while(!is_empty()) del();
+		head = tail = NULL;
+		return *this;
+	};
+	Queque& del() { // удалить элемент из начала очереди
+		assert( !is_empty() );
+		Node* tmp = head->getNext(); // запоминаем указатель на следующий элемент очереди
 		delete head; // удаляем элемент... и переводим указатель на следующий элемент
 		if(!tmp) {
 			head = tail = NULL;
@@ -35,139 +107,77 @@ public:
 		}
 		return *this;
 	};
-	Queue& clear() {
-		while(!is_empty()) del();
-		return *this;
-	};
-	Queue& push(const int& data) { // добавить элемент в конец очереди
-		Node* node = new Node();
-		node->data = data;
-		node->next = NULL;
-		if (!head) {
-			head = tail = node;
-		} else {
-			tail->next = node;
-			tail = node;
-		}
-	};
-	int pop() { // извлечь элемент из начала очереди
-		if ( !is_empty() ) {
-			int data = head->data;
-			del();
-			return data;
-		} else {
-			cout << "Стек пустой" << endl;
-			exit;
-		}
-	};
-	int& get() { //получить элемент из начала очереди
-		if ( is_empty() ) {
-			cout << "Стек пустой" << endl;
-			exit;	
-		} else {
-			return head->data;
-		}
-	}
-	int& getLast() { //получить элемент из конца очереди
-		if ( is_empty() ) {
-			cout << "Стек пустой" << endl;
-			exit;	
-		} else {
-			return tail->data;
-		}	
-	}
-	Queue& set(int& data) {
-		if ( is_empty() ) {
-			cout << "Стек пустой" << endl;
-			exit;	
-		} else {
-			head->data = data;
-			return *this;
-		}
-	};
-	Node& getn(int ind) {
-		Node *node = head;
-		int i = 0;
-		if (ind==0) return *head;
-		while (node->next) {
-			node = node->next;
-			i+=1;
-			if (i==ind) return *node;
-		}
-	}
-	friend ostream& operator<< ( ostream& o, Queue& q) {
+	friend ostream& operator<< ( ostream& o, Queque& q) {
 		if (q.is_empty()) {
-			cout << "NULL";
+			o << "NULL";
 			return o;
 		}
 		Node* node = q.head;
-		while (node->next) {
-			cout << node->data << " ";
-			node = node->next;
+		while (node->getNext()) {
+			o << node->getData() << " ";
+			node = node->getNext();
 		}
-		cout << node->data;
+		o << node->getData();
 		return o;
 	}
 };
 
-Queue sort(Queue * original) {
-	static Queue helpful;
-	// cout << *original;
-	if (original->is_empty()) {
-		cout << "The stack is empty\n";
-		exit(0);
+Queque sort(Queque* original) {
+	Queque helpful;
+	assert( !original->is_empty() );
+	Node *node = original->getFirst();
+	int max = node->getData();
+
+	while ( node->getNext() ) {
+		if (node->getData()>max) max = node->getData();
+		node = node->getNext();
 	}
-	Node *node = original->head;
-	int max = node->data;
-	while (node->next) {
-		if (node->data>max) max = node->data;
-		node = node->next;
-	}
+	if (node->getData()>max) max = node->getData();
 	for (int i=0; i<=max; i+=1) {
-		node = original->head;
+		node = original->getFirst();
 		helpful.push(0);
-		while (node->next) {
-			if (node->data==i) {
-				helpful.getLast()+=1;
+		while (node->getNext()) {
+			if (node->getData()==i) {
+				helpful.setLastData( helpful.getLastData()+1 );
 			}
-			node = node->next;
+			node = node->getNext();
 		}
-		if (node->data==i) {
-			helpful.getLast()+=1;
+		if (node->getData()==i) {
+			helpful.setLastData( helpful.getLastData()+1 );
 		}
 	}
 	original->clear();
-	node = helpful.head;
-	// cout << helpful << endl;
-	int k = 0; // счётчик;
-	while (node->next) {
-		if (node->data==0) {
-			k+=1;
-			node = node->next;
+	node = helpful.getFirst();
+	int k = -1; // счётчик;
+	cout << helpful << endl;
+
+	while (node->getNext()) {
+		k+=1;
+		if (node->getData()==0) {
+			node = node->getNext();
 			continue;
 		}
-		for (int i=1; i<=node->data; i+=1) {
+		for (int i=1; i<=node->getData(); i+=1) {
 			original->push(k);
 		}
-		k+=1;
-		node = node->next;
+		node = node->getNext();
+
 	}
-	if (node->data!=0) {
-		for (int i=1; i<=node->data; i+=1) {
-			original->push(k);
-		}
+	k+=1;
+	for (int i=1; i<=node->getData(); i+=1) {
+		original->push(k);
 	}
+	cout << *original << endl;
 	return *original;
 }
 
-int main(){
+int main() {
 	srand (time(NULL));
-	Queue queue;
-	for (int i = 0; i < 5; ++i) {
-		queue.push( rand()%10 );
+	Queque queque;
+	for (int i = 0; i < 10; ++i) {
+		queque.push( rand()%10 );
 	}
-	cout << "Original stack is: " << queue << endl;
-	queue = sort(&queue);
-	cout << "Sorted stack is: " << queue << endl;
+	cout << "Original stack is: " << queque << endl;
+	sort(&queque);
 	return 0;
-};
+}
